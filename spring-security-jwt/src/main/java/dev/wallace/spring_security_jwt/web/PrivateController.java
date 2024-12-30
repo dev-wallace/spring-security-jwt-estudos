0a3cbde33fbabf6adbb95e9d0aa90cf456744043
yokaithefato <wallacewwc2015wwk@gmail.com>
2024-12-30 11:45:30 -0300

    Add PrivateController with a greeting endpoint

--- spring-security-jwt/src/main/java/dev/wallace/spring_security_jwt/web/PrivateController.java
+++ spring-security-jwt/src/main/java/dev/wallace/spring_security_jwt/web/PrivateController.java
@@ -1,6 +1,19 @@
 package dev.wallace.spring_security_jwt.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("private")
 public class PrivateController {
+
     
+    @GetMapping
+    public String getMassage(){
+        return "Hello from private API";
+    }
+    
+
+
     
 }
